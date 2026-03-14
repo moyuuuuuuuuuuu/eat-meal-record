caae99292b54563d95af5c9e78c1f06f5a5844a2
墨鱼 <janmas-cromwell@outlook.com>
2026-03-14 13:32:39 +0800

    fix:aitoken计数器

--- app/model/UserUsageModel.php
+++ app/model/UserUsageModel.php
@@ -7,6 +7,7 @@ use app\common\base\BaseModel;
 class UserUsageModel extends BaseModel
 {
     const UPDATED_AT = null;
+    protected $table    = 'user_usages';
     protected $fillable = [
         'user_id',
         'usage',
